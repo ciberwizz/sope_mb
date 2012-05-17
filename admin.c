a91631fbb1da012d073cc66704a99bf6d56831eb
Miguel Rossi Seabra <miguelrossis@gmail.com>
2012-05-17 18:56:24 +0100

    comecar fifos

--- admin.c
+++ admin.c
@@ -1,8 +1,86 @@
 #include "util.h"
 
+// nr conta de admin =000 000 0;
+void interface();
 
-//TODO interface
+int main() {
 
-int main(){
+	interface();
+	return 0;
+}
+
+void interface(){
+    printf("interface admin\n");
+
+    // nr conta de admin =000 000 0;
+    unsigned int numconta = 0;
+    char pin[5];
+    char accao;
+
+    __fpurge(stdin);
+
+    printf("Introduza o pin da conta de administrador: ");
+    fscanf (stdin, "%s", pin);
+
+    while(strlen(pin) != 4)
+    {
+            printf("erro pin nao tem 4 caracteres\n");
+            printf("Introduza o pin da conta: ");
+            fscanf (stdin, "%s", pin);
+    }
+
+
+    printf("num da conta foi : %ud\n", numconta);
+
+    printf("pin da conta foi : %s\n", pin);
+
+    do {
+		printf("\n\n");
+		printf("Que operacao deseja efectuar?\n");
+		printf("Listar contas?     (l)\n");
+		printf("Criar nova conta?  (c)\n");
+		printf("Remover conta?     (r)\n");
+		printf("Desligar servidor? (d)\n");
+		printf("Sair?              (s)\n");
+
+		__fpurge(stdin);
+		accao = getchar();
+
+		switch(accao){
+
+//		printf("Listar contas?     (l)\n");
+		case 'l':
+		case 'L':
+			//TODO enviar comando para o fifo request e esperar pela resposta
+			break;
+
+//		printf("Criar nova conta?  (c)\n");
+		case 'c':
+		case 'C':
+			//TODO enviar comando para o fifo request e esperar pela resposta
+			break;
+
+//		printf("Remover conta?     (r)\n");
+		case 'r':
+		case 'R':
+			//TODO enviar comando para o fifo request e esperar pela resposta
+			break;
+
+//		printf("Desligar servidor? (d)\n");
+		case 'd':
+		case 'D':
+			//TODO enviar comando para o fifo request e esperar pela resposta
+			break;
+
+//		printf("Sair?              (s)\n");
+		case 's':
+		case 'S':
+			return;
+			break;
+
+		}
+
+    } while(1);
 
 }
+
--- user.c
+++ user.c
@@ -1,33 +1,58 @@
-#include "util.h"
+//#include "util.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int createFifo(char*);
+char* readFifo(char*, int);
+int writeFifo(char*, char*);
 
 
 void interface();
 
+
 int main() {
 
+	pid_t pid = getpid();
+	char spid[11];
 
+	sprintf(spid,"ans%ld",(long int) pid);
+	if( creatFifo(spid) != 0) {
+		printf("\nimpossivel criar FIFO.\n");
+		return 1;
+	}
 
+	interface();
+
+	remove(spid);
+	return 0;
 }
 
 void interface(){
     printf("interface\n");
 
     unsigned int numconta;
-    char pin[4];
+    char pin[5];
+    char accao;
 
     printf("Introduza o numero da conta: ");
-    fscanf (stdin, "%d", &numconta);
+    fscanf (stdin, "%ud", &numconta);
 
     __fpurge(stdin);
 
     printf("Introduza o pin da conta: ");
-    fscanf (stdin, "%s", &pin);
+    fscanf (stdin, "%s", pin);
 
     while(strlen(pin) != 4)
     {
             printf("erro pin nao tem 4 caracteres\n");
             printf("Introduza o pin da conta: ");
-            fscanf (stdin, "%s", &pin);
+            fscanf (stdin, "%s", pin);
     }
 
 
@@ -35,5 +60,90 @@ void interface(){
 
     printf("pin da conta foi : %s\n", pin);
 
+    do {
+		printf("\n\n");
+		printf("Que operacao deseja efectuar?\n");
+		printf("Depositar dinheiro?  (d)\n");
+		printf("Levantar dinheiro?   (l)\n");
+		printf("Transferir dinheiro? (t)\n");
+		printf("Consultar saldo?     (c)\n");
+		printf("Sair?                (s)\n");
+
+		__fpurge(stdin);
+		accao = getchar();
+
+		switch(accao){
+
+//		printf("Depositar dinheiro?  (d)\n");
+		case 'd':
+		case 'D':
+			//TODO enviar comando para o fifo request e esperar pela resposta
+			break;
+
+//		printf("Levantar dinheiro?   (l)\n");
+		case 'l':
+		case 'L':
+			//TODO enviar comando para o fifo request e esperar pela resposta
+			break;
+
+//		printf("Transferir dinheiro? (t)\n");
+		case 't':
+		case 'T':
+			//TODO enviar comando para o fifo request e esperar pela resposta
+			break;
+
+//		printf("Consultar saldo?     (c)\n");
+		case 'c':
+		case 'C':
+			//TODO enviar comando para o fifo request e esperar pela resposta
+			break;
+
+//		printf("Sair?                (s)\n");
+		case 's':
+		case 'S':
+			return;
+			break;
+
+		}
+
+    } while(1);
+
+}
+
+int createFifo(char* ff){
+	// S_IRWXU | S_IRWXO = dar todas as permissoes ao ficheiro
+	return mkfifo(ff, S_IRWXU | S_IRWXO);
+
+}
+
+char* readFifo(char* path, int timeout){
+	int fifo = open(path, O_RDONLY | O_NONBLOCK);
+	char buff[1000];
+	int start = time(), end = 0;
+
+	if(timeout == 0)
+		timeout = -1;
+
+	//erro
+	if( fifo == -1)
+		return NULL;
+
+	while( (end - start) != timeout){
+		if(read(fifo, buff, 1000) > 0 )
+			break;
+
+		//sleep for 250 ms
+		usleep(250 * 1000);
+
+		end = time();
+	}
+
+	if( end-start == 0)
+		return NULL;
+	else return buff;
+}
+
+int writeFifo(char* path, char* buff){
+ //TODO
 }
 
